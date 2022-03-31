aaf0bc06d6b03644363c481e0c58c1aa476ebe34
Just-Mike4 <77481757+Just-Mike4@users.noreply.github.com>
2022-03-31 13:36:20 +0100

    Update GPA_caculator.py

--- GPA_caculator.py
+++ GPA_caculator.py
@@ -2,24 +2,38 @@ import numpy as np
 import pandas as pd
 Number_of_courses= int(input("Number of courses:"))
 i= 0
-def Values():
+def Points():
 	if number <= 39:
-		data['values'].append(0)
+		data['points'].append(0)
 	elif number >=40 and number<=44:
-		data['values'].append(1)
+		data['points'].append(1)
 	elif number >=45 and number <=49:
-		data['values'].append(2)
+		data['points'].append(2)
 	elif number>=50 and number<=59:
-		data['values'].append(3)
+		data['points'].append(3)
 	elif number>=60 and number<=69:
-		data['values'].append(4)
+		data['points'].append(4)
 	elif number>=70 and number<=100:
-		data['values'].append(5)
+		data['points'].append(5)
+def Remark():
+	if Average_2sf  <= 1:
+		print("Fail")
+	elif Average_2sf >= 1 and Average_2sf <= 1.49:
+		print("Pass")
+	elif Average_2sf >= 1.5 and Average_2sf <=2.39 :
+		print ("3rd Class")
+	elif Average_2sf >= 2.40 and Average_2sf <= 3.49:
+		print("2nd Class Lower")
+	elif Average_2sf >= 3.50 and Average_2sf <= 4.49:
+		print ("2nd Class Upper")
+	elif Average_2sf >= 4.50 and Average_2sf <= 5.00:
+		print("First Class")
+		
 data={
 		'courses': [],
 		'units':[],
 		'scores':[],
-		'values':[]
+		'points':[]
 }
 while Number_of_courses != i:
 	course= input("Enter a course:")
@@ -30,13 +44,13 @@ while Number_of_courses != i:
 	data['scores'].append(scores)
 	i+=1
 for number in data['scores']:
-	Values()
+	Points()
 df= pd.DataFrame(data)
 print(df)
 sum_of_units=df['units'].sum()
-values_times_units = df['values']*df['units']
-Sum= values_times_units.sum()
+Points_times_units = df['points']*df['units']
+Sum= Points_times_units.sum()
 Average= Sum/sum_of_units
-Average_2sf= "{:.2f}".format(Average)
+Average_2sf=float("{:.2f}".format(Average))
 print("cgpa=",Average_2sf)
-
+Remark()
